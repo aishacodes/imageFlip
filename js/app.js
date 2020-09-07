0d197e47cd9ed7d7efb77f0390083e4e5d365618
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-09-08 00:28:21 +0100

    feat: image flip

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(".flip-card").click(function() {
+    $(this).toggleClass("card-rotate");
+});
+$(".flip-card").hover(function() {
+        $(this).children(".front").children('.p').css('z-index', '1');
+        $(this).children(".front").children('.p').css('transition', '1.5s linear');
+        // $(".p").css('z-index', '1');
+        // $(".p").css('transition', ' 1.5s linear');
+    },
+    function() {
+        $(this).children(".front").children('.p').css('z-index', '-1');
+        $(this).children(".front").children('.p').css('transition', ' 0.2s ease-out');
+
+        // $(".p").css('z-index', '-1');
+        // $(".p").css('transition', ' 0.2s ease-out');
+    });
+$(".flip-card").hover(function() {
+        $(this).children(".front").children('p').css('bottom', '2rem');
+        $(this).children(".front").children('p').css('transition', ' 0.5s linear');
+    },
+    function() {
+        $(this).children(".front").children('p').css('bottom', '0');
+        $(this).children(".front").children('p').css('transition', ' 0.5s linear');
+
+    });
