fa9798a23335aaa92c41fa00c0760ed16556f8ac
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-09-09 08:39:29 +0100

    updated codebase

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
-$(".flip-card").click(function() {
-    $(this).toggleClass("card-rotate");
+// $(".flip-card").click(function() {
+
+//     $(this).toggleClass("card-rotate");
+// });
+$('.flip-card').click(function(e) {
+    if ($(this).hasClass('card-rotate')) {
+        $(".flip-card").removeClass('card-rotate');
+        $(this).toggleClass('card-rotate');
+    } else {
+        $('.flip-card').removeClass('card-rotate');
+    }
+    $(this).toggleClass('card-rotate');
 });
+
+
 $(".flip-card").hover(function() {
         $(this).children(".front").children('.p').css('z-index', '1');
         $(this).children(".front").children('.p').css('transition', '1.5s linear');
